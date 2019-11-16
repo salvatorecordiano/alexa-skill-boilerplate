d00342fcddd3040d6fb13d737bdc7ae5858597bc
Salvatore Cordiano <salvatorecordiano@users.noreply.github.com>
2019-11-16 09:58:03 +0100

    Make build using webpack (#1)
    * Make build using webpack

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 docker run --rm --name alexa-skill -v `pwd`/project:/alexa-skill -w /alexa-skill node:13.1.0-stretch-slim yarn build
+cd project
+rm -fr dist || true
+cd ..
 rm -fr deploy || true
 rm deploy.zip || true
 mkdir deploy
 cp -R ./project/dist/* ./deploy
-cp -R ./project/node_modules ./deploy
-cp ./project/package.json ./deploy
 cd deploy && zip -r ../deploy.zip . && cd ..
 rm -fr deploy || true
