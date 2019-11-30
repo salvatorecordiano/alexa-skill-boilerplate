330f931e14ecceb7e0d245f13203caceac05c5c3
Salvatore Cordiano <salvatore.cordiano@facile.it>
2019-11-30 14:12:38 +0100

    Improve scripts

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 rm -fr deploy || true
 rm -fr project/dist || true
 mkdir -p project/dist && touch project/dist/.gitkeep
-docker run --rm --name alexa-skill -v `pwd`/project:/alexa-skill -w /alexa-skill node:13.1.0-stretch-slim yarn build
+docker run --rm -v `pwd`/project:/alexa-skill -w /alexa-skill node:13.1.0-stretch-slim yarn build
 mkdir deploy
 cp -R ./project/dist/* ./deploy
 cd deploy && zip -r ../deploy.zip . && cd ..
--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --name alexa-skill -v `pwd`/project:/alexa-skill -w /alexa-skill node:13.1.0-stretch-slim yarn
+docker run --rm -v `pwd`/project:/alexa-skill -w /alexa-skill node:13.1.0-stretch-slim yarn
